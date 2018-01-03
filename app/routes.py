32f67baa1fce529d46d6d765daebc1bdb9daefa5
egkurilov <egkurilov@gmail.com>
2018-01-03 21:43:59 +0300

    ///

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,18 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 from .message import Message
 import logging
+
 
 bot = Blueprint('bot', __name__)
 logger = logging.getLogger(__name__)
 
 
-@bot.route("/bot", )
+@bot.route("/bot")
 def bot_message():
     body = request.get_json()
-    logger.debug(str(body))
-    message = Message(body)
-    logger.debug(repr(message))
-    return message
+    print(body)
+    #logger.debug(str(body))
+    #message = Message(body)
+    #logger.debug(repr(message))
+
+    return jsonify(body)
