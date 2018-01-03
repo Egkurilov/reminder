a9f4f3fda92debf2c68a052ac6e03e6bd771c4a0
egkurilov <egkurilov@gmail.com>
2018-01-03 20:39:21 +0300

    ///

--- start_web.py
+++ start_web.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
-
+import logging
 from flask import Flask
 
 app = Flask(__name__)
 
 
 if __name__ == "__main__":
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
     app.run(host='10.8.54.139', port=88)
