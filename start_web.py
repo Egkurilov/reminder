c55ec3230d13bfb0488df79770e144d35e33ec96
egkurilov <egkurilov@gmail.com>
2018-01-03 21:49:15 +0300

    ///

--- start_web.py
+++ start_web.py
@@ -18,4 +18,4 @@ if __name__ == "__main__":
     ch.setFormatter(formatter)
     logger.addHandler(ch)
 
-    app.run(host='localhost', port=88, debug=True)
+    app.run(host='0.0.0.0', port=88, debug=True)
