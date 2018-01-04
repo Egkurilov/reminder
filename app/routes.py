aa1616566bbb14d879ed69ada44948ac5a823fce
egkurilov <egkurilov@gmail.com>
2018-01-04 11:19:30 +0300

    ///

--- app/routes.py
+++ app/routes.py
@@ -9,6 +9,7 @@ logger = logging.getLogger(__name__)
 
 @bot.route("/bot", methods=['POST'])
 def bot_message():
+    #TODO вынести логику логирования в отдельный класс
     body = request.get_json()
     message = Message(body)
     logger.debug(repr(message))
