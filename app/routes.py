0ef8d27128d8b2278a691c48abd58300105e287e
egkurilov <egkurilov@gmail.com>
2018-01-04 11:23:58 +0300

    ///

--- app/routes.py
+++ app/routes.py
@@ -2,16 +2,16 @@ from flask import Blueprint, request, jsonify
 from .message import Message
 import logging
 
-
 bot = Blueprint('bot', __name__)
 logger = logging.getLogger(__name__)
 
 
 @bot.route("/bot", methods=['POST'])
 def bot_message():
-    #TODO вынести логику логирования в отдельный класс
+    # TODO вынести логику логирования в отдельный класс (и починить его)
     body = request.get_json()
     message = Message(body)
     logger.debug(repr(message))
-
     return jsonify(body)
+
+    # TODO Добавить роутинг для ui 
